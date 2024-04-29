7d82386dcff3623dc68f9fdf9d9491677094dfea
FreeFly0125 <simonbergman516@gmail.com>
2024-04-29 10:12:21 +0200

    hook for long latency response

--- your_api/index.js
+++ your_api/index.js
@@ -11,10 +11,35 @@ const webhookUrl = process.env.WEBHOOK || "http://localhost:3200/hookTxStatus";
 
 const transactions = {};
 
+const simulateLatency = (latency) => {
+  return new Promise((fn) => setTimeout(fn, latency));
+};
+
+const checkTxStatus = (id) => {
+  if (!transactions[id]) return;
+  if (transactions[id] === "completed" || transactions[id] === "declined")
+    return;
+  axios
+    .get(`${thirdPartyUrl}/transaction/${id}`)
+    .then((response) => {
+      const { id, status } = response.data;
+      if (transactions[id] === status) return;
+      axios
+        .put(`${clientUrl}/transaction`, { status: { id, status } })
+        .catch(() => {
+          console.log("A Error with calling client.");
+        });
+    })
+    .catch((e) => {
+      if (e.response.status == 404) {
+        return;
+      }
+      console.log(`Could not check transaction status for ${id}`);
+    });
+};
+
 app.post("/transaction", (req, res) => {
   const id = req.body.id;
-
-  console.log(`Request transaction process with id = ${req.body.id}`);
 
   if (transactions[id] === "pending") {
     res.send({ id: id, status: "pending" });
@@ -49,10 +74,22 @@ app.post("/transaction", (req, res) => {
         console.log("Error with calling third party.");
       }
     });
+
+  simulateLatency(120_000).then(() => checkTxStatus(id));
 });
 
 app.post("/hookTxStatus", (req, res) => {
-  console.log("Hook for long latency response");
+  const { id, status } = req.body;
+  if (transactions[id] === status) {
+    res.send();
+    return;
+  }
+  transactions[id] = status;
+  axios
+    .put(`${clientUrl}/transaction`, { status: { id, status } })
+    .catch((e) => {
+      console.log("A Error with calling client.", e.response.status);
+    });
   res.send();
 });
 
